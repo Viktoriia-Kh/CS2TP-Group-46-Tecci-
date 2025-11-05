d811584f179d36516e359b95834d45a3938afb39
Manrohit Bahia <msbahia11@outlook.com>
2025-11-05 20:01:11 +0000

    created the basic html structure for the front end of the login page

---
+++ loginpage/resources/views/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+        <link rel="stylesheet" href="{{asset('css/ style.css') }}"> <!-- created a link to the stylesheet once created so styling applies to the page -->
+    </head>
+    <body>
+
+
+    </body>
+
+</html>
