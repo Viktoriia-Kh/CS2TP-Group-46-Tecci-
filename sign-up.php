99abfa8cc0d37b32b88634e68b6fc7328c7e8adc
ibrahim3103 <muhammadibrahim9087654@gmail.com>
2025-11-09 11:05:17 +0000

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -164,11 +164,4 @@ export default function App() {
               <span className="text-gray-600">Already have an account? </span>
               <a href="#" className="text-blue-600 hover:underline">
                 Sign in
-              </a>
-            </div>
-          </CardContent>
-        </Card>
-      </div>
-    </div>
-  );
-}
+             
