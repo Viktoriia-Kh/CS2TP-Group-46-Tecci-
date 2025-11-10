6776a900d5c7535ff7cf80b3a98fe893ac2ce748
Manrohit Bahia <msbahia11@outlook.com>
2025-11-10 20:28:21 +0000

    started the backend controller implementation, initialised some variables and added a small change to the blade front end file

--- loginpage/app/Http/Controllers/LoginController.php
+++ loginpage/app/Http/Controllers/LoginController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        session_start(); // once the user has logged in the session will start
+
+        // this will hold the email address which the user enters when logging in
+        $email_address = '';
+
+        // this will hold the password entered by the user when they log in
+        $password = '';
+
+        // this will show any error messages if something goes wrong during the login (e.g., wrong password)
+        $error_messages = [];
+    }
 }
--- loginpage/resources/views/login.blade.php
+++ loginpage/resources/views/login.blade.php
@@ -6,7 +6,9 @@
         <link rel="stylesheet" href="{{asset('css/ style.css') }}"> <!-- created a link to the stylesheet once created so styling applies to the page -->
     </head>
     <body>
-
+        <section class="section-form">
+            <h2>Login</h2>
+        </section>
 
     </body>
 
