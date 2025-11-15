04a36dd10a4c1e884757ee1d201385f418adda8e
Manrohit Bahia <msbahia11@outlook.com>
2025-11-15 15:33:21 +0000

    Finished off the backend and made changes to use another laravel framework for robustness. Added use\Illuminate\Support\Facades\Auth. Added the final verification and the user is directed to either the homepage or remains in the login with an error message.

--- loginpage/app/Http/Controllers/LoginController.php
+++ loginpage/app/Http/Controllers/LoginController.php
@@ -4,21 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB; // enables me to use database queries
+use Illuminate\Support\Facades\Hash; // for password hashing
+use Illuminate\Support\Facades\Auth; // checks the user credentials and starts a session
 
 class LoginController extends Controller
 {
     public function login(Request $request)
     {
-        session_start(); // once the user has logged in the session will start
 
-        // this will hold the email address which the user enters when logging in
-        $email_address = '';
-
-        // this will hold the password entered by the user when they log in
-        $password = '';
-
-        // this will show any error messages if something goes wrong during the login (e.g., wrong password)
-        $error_messages[] = '';
+        // assigning variables
+        $email_address = ''; // this will hold the email address which the user enters when logging in
+        $password = ''; // this will hold the password entered by the user when they log in
+        $error_messages = []; // this will show any error messages if something goes wrong during the login (e.g., wrong password)
 
         // this will check if the form has been submitted by the user
         if ($request->isMethod('post')) {
@@ -37,10 +34,19 @@ class LoginController extends Controller
 
             // check if there are no errors during the login process
             if (empty($error_messages)) {
-                $user = DB::table('users')->where('email', $email_address)->first(); // checks if the user exists in the database
+                $user_credentials = ['email' => $email_address, 'password' => $password]; // the user inputs are ready to be checked
+
+                // here it will attempt to log the user in
+                if (Auth::attempt(['email' => $email_address, 'password' => $password])) {
+                    $request->session()->regenerate(); // the session ID is reset for security purposes
+                    return redirect()->intended('/homepage'); // this will redirect the user to the homepage
+                } else {
+                    $error_messages[] = "Please login with the correct details."; // the user will get this message if the login does not work
+                }
+
             }
 
         }
-
+        return view('login', ['error_messages' => $error_messages]); // if the login was unsuccessful the user sees the login page with the error message
     }
 }
