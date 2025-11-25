396136e7129b691a80965e5312db0949650afbc7
Viktoriia-Kh <viktoriia.kchenko@gmail.com>
2025-11-25 16:22:28 +0000

    Added basic ui for display product page

---
+++ resources/views/displayproduct.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tecci</title>
+    <link rel="stylesheet" href="{{asset('style.css')}}">
+</head>
+<body>
+    <header>
+    <div class="header-top">
+        <a href="#"><img src="{{asset('images/logo.png')}}" alt="Tecci logo" class="logo"></a>
+        <div class="header-icons">
+            <a href="#"><img src="{{asset('images/user.png')}}" alt="User Link" class="img"></a>
+            <a href="#"><img src="{{asset('images/trolley.png')}}" alt="Products Link" class="img"></a>
+        </div>
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Sign Up</a></li>
+        </ul>
+    </nav>
+</header>
+
+    <main>
+    </main>
+
+    <footer>
+        <p>&copy; 2025 Tecci. All Rights Reserved.</p>
+    </footer>
+</body>
+</html>
