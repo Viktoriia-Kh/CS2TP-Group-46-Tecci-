e48c4119dd6393b2cf3da3649a10b05dc2dfa8ac
ibrahim3103 <muhammadibrahim9087654@gmail.com>
2025-11-25 17:01:14 +0000

    making confirm password and password line up

--- sign_up/resources/views/signup.blade.php
+++ sign_up/resources/views/signup.blade.php
@@ -83,11 +83,9 @@
           <div>
             <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
             <input type="password" name="password" id="password" required class="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
-          </div>
-          </div>  
            <label for ="password_confirmation" class="block text-sm font-medium text-gray-700">Confirm Password</label>
             <input type="password" name="password_confirmation" id="password_confirmation" required class="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
-          <div></div>
+        </div>
 
           <div>
             <button type="submit" class="w-full rounded-md bg-indigo-600 px-4 py-2 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm">
