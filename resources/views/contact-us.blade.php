9d704b28a42bde9d3ea4b8734c9d2bfa633813e9
Ramsha Ahmed <rahmed0204@outlook.com>
2025-11-26 19:39:43 +0000

    Added Default HTML Code And Links To CSS File, Fonts And Icons

---
+++ resources/views/contact-us.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Tecci | Contact Us</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!--Link to CSS File for Contact Page-->
+  <link rel="stylesheet" href="contactstyle.css" />
+  <!--Google Font-->
+  <link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet" />
+  <!--Font Awesome for Icons-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+
+</head> 
