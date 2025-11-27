dbb1ca9cf528054725dc9f0bafdcfd5d4232ff4c
Ramsha Ahmed <rahmed0204@outlook.com>
2025-11-27 00:23:26 +0000

    Fixed Slight Error In HTML Header Class, Added New CSS Link, Added Text In Footer (Changed It From Sample Dummy Text To Actual Content), And Added Actual Contact Info In The Footer

--- resources/views/home-page-2.blade.php
+++ resources/views/home-page-2.blade.php
@@ -1,105 +1,98 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8" />
   <title>Tecci | Affordable Tech for Students</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <!--Links to HTML/CSS Files-->
   <link rel="stylesheet" href="homestyle.css" />
-  <link rel="stylesheet" href="NewContactStyles.css"/>
+  <link rel="stylesheet" href="contactstyle.css" />
   <!--Google Font-->
   <link href='https://fonts.googleapis.com/css?family=Signika' rel='stylesheet'>
   <!--Font Awesome for Icons-->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
 
 </head>
 
 <body>
-  header class="main-header">
-  <div class="container nav-container">
-    <!-- Logo -->
-     <a href="NewHome.html" class="logo"> <!--Using this will make the Logo clickable and takes the user to the Home Page-->
-      <img src="https://i.ibb.co/8tB48xb/Logo.png" alt="Tecci logo">
-      <span class="logo-text">TECCI</span> <!--Is an inline element used for short text-->
-    </a>
-    
-    <!--Navigation Menu-->
-    <nav class="main-nav">
-      <ul>
-        <li><a href="index.html" class="active">Home</a></li> <!--class="active" marks the Current Page-->
-        <li><a href="about.html">About</a></li>
-        <li><a href="NewContact.html">Contact</a></li>
-        <li><a href="products.html">Products</a></li>
-      </ul>
-    </nav>
+  <header class="main-header">
+    <div class="container nav-container">
 
-    <!--Icons-->
-    <div class="nav-icons">
-      <a href="wishlist.html"><i class="fa-regular fa-heart"></i></a> <!--fa-heart is a Heart Icon linked from Font Awesome-->
-      <a href="basket.html"><i class="fa-solid fa-cart-shopping"></i></a> <!--fa-cart-shopping is a Shopping Cart Icon linked from Font Awesome-->
-      <a href="account.html"><i class="fa-regular fa-user"></i></a> <!--fa-user is a User Icon linked from Font Awesome-->
+      <!-- Logo -->
+      <a href="home-page-2.blade.php" class="logo"> <!--Using this will make the Logo clickable and takes the user to the Home Page-->
+        <img src="https://i.ibb.co/8tB48xb/Logo.png" alt="Tecci logo">
+        <span class="logo-text">TECCI</span> <!--Is an inline element used for short text-->
+      </a>
+
+      <!--Navigation Menu-->
+      <nav class="main-nav">
+        <ul>
+          <li><a href="index.html" class="active">Home</a></li> <!--class="active" marks the Current Page-->
+          <li><a href="about.html">About</a></li>
+          <li><a href="contact-us-2.blade.php">Contact</a></li>
+          <li><a href="products.html">Products</a></li>
+        </ul>
+      </nav>
+
+      <!--Icons-->
+      <div class="nav-icons">
+        <a href="wishlist.html"><i class="fa-regular fa-heart"></i></a> <!--fa-heart is a Heart Icon linked from Font Awesome-->
+        <a href="basket.html"><i class="fa-solid fa-cart-shopping"></i></a> <!--fa-cart-shopping is a Shopping Cart Icon linked from Font Awesome-->
+        <a href="account.html"><i class="fa-regular fa-user"></i></a> <!--fa-user is a User Icon linked from Font Awesome-->
+      </div>
     </div>
-  </div>
-</header>
+  </header>
 
 
-<!--FOOTER-->
-<footer class="site-footer">
-  <div class="container footer-inner"> <!--footer-inner used to create multi-column layout-->
-    <div class="footer-col">
-      <h3>TECCI</h3>
-      <p>
-        Lorem Ipsum Lorem Ipsum Lorem<br>
-        Ipsum Lorem Ipsum
-      </p>
-    
+  <!--FOOTER-->
+  <footer class="site-footer">
+    <div class="container footer-inner"> <!--footer-inner used to create multi-column layout-->
+      <div class="footer-col">
+        <h3>TECCI</h3>
+        <p>
+          Smart Tech at Smart Prices.<br>
+          Tecci makes premium devices accessible to<br>
+          students and customers across the UK.
+        </p>
+
+      </div>
+
+      <div class="footer-col">
+        <h4>Quick Links</h4>
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="contact.html">Contact</a></li>
+          <li><a href="products.html">Products</a></li>
+          <li><a href="basket.html">Basket</a></li>
+          <li><a href="account.html">My Account</a></li>
+        </ul>
+      </div>
+
+      <div class="footer-col">
+        <h4>Contact Info</h4>
+        <ul class="contact-list">
+          <li>
+            <i class="fa-solid fa-location-dot"></i> <!--fa-loocation-dot is a Location Icon linked from Font Awesome-->
+            <span>0121 555 0198</span><br><br>
+          </li>
+          <li>
+            <i class="fa-solid fa-phone"></i> <!--fa-phone is a Phone Icon linked from Font Awesome-->
+            <span>Tecci_Queries@net.com</span><br><br>
+          </li>
+          <li>
+            <i class="fa-regular fa-envelope"></i> <!--fa-envelope is an Envelope Icon linked from Font Awesome-->
+            <span>Birmingham, B4 7ET</span><br><br>
+          </li>
+        </ul>
+      </div>
+    </div> <!--Closes <div class="container footer-inner"-->
+    <div class="footer-bottom">
+      <p>&copy; 2025 Tecci. All rights reserved.</p>
     </div>
-                
-    <div class="footer-col">
-      <h4>Quick Links</h4>
-      <ul>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="contact.html">Contact</a></li>
-        <li><a href="products.html">Products</a></li>
-        <li><a href="basket.html">Basket</a></li>
-        <li><a href="account.html">My Account</a></li>
-      </ul>
-    </div>
+  </footer>
 
-    <div class="footer-col">
-      <h4>Contact Info</h4>
-      <ul class="contact-list">
-        <li>
-          <i class="fa-solid fa-location-dot"></i> <!--fa-loocation-dot is a Location Icon linked from Font Awesome-->
-          <span>Lorem Ipsum Blah</span><br><br>
-        </li>
-        <li>
-          <i class="fa-solid fa-phone"></i> <!--fa-phone is a Phone Icon linked from Font Awesome-->
-          <span>Lorem Ipsum Blah 2</span><br><br>
-        </li>
-        <li>
-          <i class="fa-regular fa-envelope"></i> <!--fa-envelope is an Envelope Icon linked from Font Awesome-->
-        </li>
-      </ul>
-    </div> 
-  </div> <!--Closes <div class="container footer-inner"-->
-  <div class="footer-bottom">
-    <p>&copy; 2025 Tecci. All rights reserved.</p>
-  </div>
-</footer>
-    
-    </body>
+</body>
+
 </html>
-  
-
-
-
-
-
-
-
-
-
-
-
