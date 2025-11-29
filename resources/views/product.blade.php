77558ac49483eac3feed0a651de01856f571f88c
Viktoriia-Kh <viktoriia.kchenko@gmail.com>
2025-11-29 21:15:52 +0000

    Updated header and footer of the webpage to match new design guidelines.

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,31 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tecci</title>
-    <link rel="stylesheet" href="{{asset('style.css')}}">
+  <meta charset="UTF-8" />
+  <title>Tecci | Affordable Tech for Students</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!--Links to HTML/CSS Files-->
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="contactstyle.css" />
+  <!--Google Font-->
+  <link href='https://fonts.googleapis.com/css?family=Signika' rel='stylesheet'>
+  <!--Font Awesome for Icons-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+
 </head>
 <body>
-    <header>
-    <div class="header-top">
-        <a href="#"><img src="{{asset('images/logo.png')}}" alt="Tecci logo" class="logo"></a>
-        <div class="header-icons">
-            <a href="#"><img src="{{asset('images/user.png')}}" alt="User Link" class="img"></a>
-            <a href="#"><img src="{{asset('images/trolley.png')}}" alt="Products Link" class="img"></a>
-        </div>
-    </div>
+    <header class="main-header">
+    <div class="container nav-container">
 
-    <nav>
+      <!-- Logo -->
+      <a href="home-page-2.blade.php" class="logo"> <!--Using this will make the Logo clickable and takes the user to the Home Page-->
+        <img src="https://i.ibb.co/8tB48xb/Logo.png" alt="Tecci logo">
+        <span class="logo-text">TECCI</span>
+      </a>
+
+      <!--Navigation Menu-->
+      <nav class="main-nav">
         <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Contact Us</a></li>
-            <li><a href="#">Products</a></li>
-            <li><a href="#">Sign Up</a></li>
+          <li><a href="index.html" class="active">Home</a></li> <!--class="active" marks the Current Page-->
+          <li><a href="about.html">About</a></li>
+          <li><a href="contact-us-2.blade.php">Contact</a></li>
+          <li><a href="products.html">Products</a></li>
         </ul>
-    </nav>
-</header>
+      </nav>
+
+      <!--Icons-->
+      <div class="nav-icons">
+        <a href="wishlist.html"><i class="fa-regular fa-heart"></i></a> <!--fa-heart is a Heart Icon linked from Font Awesome-->
+        <a href="basket.html"><i class="fa-solid fa-cart-shopping"></i></a> <!--fa-cart-shopping is a Shopping Cart Icon linked from Font Awesome-->
+        <a href="account.html"><i class="fa-regular fa-user"></i></a> <!--fa-user is a User Icon linked from Font Awesome-->
+      </div>
+    </div>
+  </header>
 
     <main>
         <section class="product-detail">
@@ -53,8 +68,52 @@
         </section>
     </main>
 
-    <footer>
-        <p>&copy; 2025 Tecci. All Rights Reserved.</p>
-    </footer>
+    <!--FOOTER-->
+  <footer class="site-footer">
+    <div class="container footer-inner"> <!--footer-inner used to create multi-column layout-->
+      <div class="footer-col">
+        <h3>TECCI</h3>
+        <p>
+          Smart Tech at Smart Prices.<br>
+          Tecci makes premium devices accessible to<br>
+          students and customers across the UK.
+        </p>
+
+      </div>
+
+      <div class="footer-col">
+        <h4>Quick Links</h4>
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="contact.html">Contact</a></li>
+          <li><a href="products.html">Products</a></li>
+          <li><a href="basket.html">Basket</a></li>
+          <li><a href="account.html">My Account</a></li>
+        </ul>
+      </div>
+
+      <div class="footer-col">
+        <h4>Contact Info</h4>
+        <ul class="contact-list">
+          <li>
+            <i class="fa-solid fa-location-dot"></i> <!--fa-loocation-dot is a Location Icon linked from Font Awesome-->
+            <span>0121 555 0198</span><br><br>
+          </li>
+          <li>
+            <i class="fa-solid fa-phone"></i> <!--fa-phone is a Phone Icon linked from Font Awesome-->
+            <span>Tecci_Queries@net.com</span><br><br>
+          </li>
+          <li>
+            <i class="fa-regular fa-envelope"></i> <!--fa-envelope is an Envelope Icon linked from Font Awesome-->
+            <span>Birmingham, B4 7ET</span><br><br>
+          </li>
+        </ul>
+      </div>
+    </div> 
+    <div class="footer-bottom">
+      <p>&copy; 2025 Tecci. All rights reserved.</p>
+    </div>
+  </footer>
 </body>
 </html>
