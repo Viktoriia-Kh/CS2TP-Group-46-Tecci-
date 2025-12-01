3eed602210341d34efd77fa75d109e3f23e80ee4
Ramsha Ahmed <rahmed0204@outlook.com>
2025-12-01 23:53:49 +0000

    Added Default HTML Code And Links To CSS File, Fonts And Icons. Also Created CSS Page For About Us Web Page

---
+++ resources/views/About-Us-2.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Tecci | Affordable Tech for Students</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--Links to HTML/CSS Files-->
+    <link rel="stylesheet" href="homestyle.css" />
+    <link rel="stylesheet" href="contactstyle.css" />
+    <link rel="stylesheet" href="Aboutstyle.css" />
+    <!--Google Font-->
+    <link href='https://fonts.googleapis.com/css?family=Signika' rel='stylesheet'>
+    <!--Font Awesome for Icons-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+
+</head>
+
+<body>
+    <header class="main-header">
+        <div class="container nav-container">
+
+            <!-- Logo -->
+            <a href="home-page-2.blade.php" class="logo">
+                <!--Using this will make the Logo clickable and takes the user to the Home Page-->
+                <img src="https://i.ibb.co/8tB48xb/Logo.png" alt="Tecci logo">
+                <span class="logo-text">TECCI</span> <!--span is an inline element used for short text-->
+            </a>
+
+            <!--Navigation Menu-->
+            <nav class="main-nav">
+                <ul>
+                    <li><a href="home-page-2.blade.php">Home</a></li> <!--class="active" marks the Current Page-->
+                    <li><a href="About-Us-2.blade.php" class="active">About</a></li>
+                    <li><a href="contact-us.blade.php">Contact</a></li>
+                    <li><a href="products.html">Products</a></li>
+                </ul>
+            </nav>
+
+            <!--Icons-->
+            <div class="nav-icons">
+                <a href="wishlist.html"><i class="fa-regular fa-heart"></i></a> <!--fa-heart is a Heart Icon linked from Font Awesome-->
+                <a href="basket.html"><i class="fa-solid fa-cart-shopping"></i></a> <!--fa-cart-shopping is a Shopping Cart Icon linked from Font Awesome-->
+                <a href="account.html"><i class="fa-regular fa-user"></i></a> <!--fa-user is a User Icon linked from Font Awesome-->
+            </div>
+
+        </div>
+    </header>
