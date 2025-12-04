9d1e7aa0c855ab893c306a69ac13c99a72866f25
KP Sangha <39467784+KirpalSangha@users.noreply.github.com>
2025-12-04 16:44:31 +0000

    Fixed server error and added backend logic for basket
    resolved the 500 error message for basket page and included 'add' 'increment' and 'delete' functionality

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -6,8 +6,52 @@ use Illuminate\Http\Request;
 
 class BasketController extends Controller
 {
+    // Show the Basket Page
     public function index()
     {
-        return view('basket'); // locates resources/views/basket.blade.php
+        // Get the basket data from the session
+        // If it doesn't exist yet, we use an empty list []
+        $basket = session()->get('basket', []);
+        
+        
+        return view('basket', compact('basket'));
+    }
+
+    // Add Item Logic
+    public function add($id)
+    {
+        $basket = session()->get('basket', []);
+
+        // Logic - Check if item exists, if yes, add +1 to quantity
+        if(isset($basket[$id])) {
+            $basket[$id]['quantity']++;
+        } else {
+            
+            $basket[$id] = [
+                "name" => "Demo Product " . $id,
+                "quantity" => 1,
+                "price" => 20.00,
+                "image" => "https://via.placeholder.com/150"
+            ];
+        }
+
+        // Save the updated list back to the browser memory
+        session()->put('basket', $basket);
+        
+        // Refresh the page so you see the new item
+        return redirect()->back(); 
+    }
+
+    
+    public function remove($id)
+    {
+        $basket = session()->get('basket');
+
+        if(isset($basket[$id])) {
+            unset($basket[$id]);
+            session()->put('basket', $basket);
+        }
+
+        return redirect()->back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
-use App\Http\Controllers\BasketController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BasketController; 
 
+// Root URL
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/basket', [BasketController::class, 'index']);
+// Homepage (where user clicks "Return to Home")
+Route::get('/homepage', function () {
+    return view('home-page-2');
+});
+
+// Basket Logic (Viewing, Adding, Removing)
+Route::get('/basket', [BasketController::class, 'index'])->name('basket.index');
+Route::get('/add-to-basket/{id}', [BasketController::class, 'add'])->name('basket.add');
+Route::get('/remove-from-basket/{id}', [BasketController::class, 'remove'])->name('basket.remove');
