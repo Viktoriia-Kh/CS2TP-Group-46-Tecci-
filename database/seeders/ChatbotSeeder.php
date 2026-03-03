64511f73b61101b2b919658300d49547b376fdd0
ibrahim3103 <muhammadibrahim9087654@gmail.com>
2026-03-03 14:16:58 +0000

    added more categories and Questions

--- database/seeders/ChatbotSeeder.php
+++ database/seeders/ChatbotSeeder.php
@@ -16,6 +16,9 @@ class ChatbotSeeder extends Seeder
 
         $refunds = ChatCategory::create(['title' => 'Refunds', 'sort_order' => 1]);
         $delivery = ChatCategory::create(['title' => 'Delivery', 'sort_order' => 2]);
+        $Account = ChatCategory::create(['title' => 'Account', 'sort_order' => 3]);
+        $Payment = ChatCategory::create(['title' => 'Payment', 'sort_order' => 4]);
+
 
         ChatFaq::create([
             'category_id' => $refunds->id,
@@ -37,5 +40,68 @@ class ChatbotSeeder extends Seeder
             'answer' => 'Standard delivery takes 2–4 working days. Express delivery is next-day.',
             'sort_order' => 1
         ]);
+
+                ChatFaq::create([
+            'category_id' => $delivery->id,
+            'question' => 'Do you deliver internationally?',
+            'answer' => 'Yes, we deliver to most countries worldwide.',
+            'sort_order' => 2   
+        ]);
+
+
+                ChatFaq::create([
+            'category_id' => $Account->id,
+            'question' => 'How do I reset my password?',
+            'answer' => 'You can reset your password by clicking "Forgot Password" on the login page.',
+            'sort_order' => 1
+        ]);
+
+                ChatFaq::create([
+            'category_id' => $Account->id,
+            'question' => 'How do I update my account information?',
+            'answer' => 'You can update your account information in the "My Account" section.',
+            'sort_order' => 2
+        ]);
+
+
+
+          ChatFaq::create([
+            'category_id' => $Account->id,
+            'question' => 'How do I update my account information?',
+            'answer' => 'You can update your account information in the "My Account" section.',
+            'sort_order' => 2
+        ]);
+
+
+
+
+
+          ChatFaq::create([
+            'category_id' => $Payment->id,
+            'question' => 'How do I update my payment information?',
+            'answer' => 'You can update your payment information in the "My Account" section.',
+            'sort_order' => 1
+        ]);
+
+
+
+
+          ChatFaq::create([
+            'category_id' => $Payment->id,
+            'question' => 'What payment methods do you accept?',
+            'answer' => 'We accept all major credit cards, PayPal, and Apple Pay.',
+            'sort_order' => 2
+        ]);
+
+
+
+
+          ChatFaq::create([
+            'category_id' => $Payment->id,
+            'question' => 'How do I cancel my payment?',
+            'answer' => 'You can cancel your payment in the "My Account" section.',
+            'sort_order' => 3
+        ]);
+
     }
 }
