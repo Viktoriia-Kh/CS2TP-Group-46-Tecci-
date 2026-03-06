c8e02d2d51e3346e53135db1ba9fe8e51e3ec633
Manrohit Bahia <msbahia11@outlook.com>
2026-03-06 20:33:08 +0000

    started developing the reset password page, added links to stylesheets

---
+++ resources/views/auth/reset-password.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Reset Your Password</title>
+        <link rel="stylesheet" href="loginstyle.css"> <!-- added a link to the stylesheet-->
+        <link rel="stylesheet" href="common-style.css">
+        <!-- Google font-->
+        <link href='https://fonts.googleapis.com/css?family=Signika' rel='stylesheet'>
+        <!-- Font awesome for icons-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    </head>
+
+
+</html>
