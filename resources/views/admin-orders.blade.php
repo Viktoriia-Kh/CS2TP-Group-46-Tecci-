5fd0f919a295dccfca124ad23fd066688938cfe0
Ramsha Ahmed <rahmed0204@outlook.com>
2026-03-14 16:09:32 +0000

    Added HTML For The Admin Header Controls Which Includes The Search Bar

--- resources/views/admin-orders.blade.php
+++ resources/views/admin-orders.blade.php
@@ -34,3 +34,10 @@
             <!--id="menuBtn" connects to the JS, for it to work-->
             <i class="fa-solid fa-bars"></i> <!--fa-bars is a Menu Icon linked from Font Awesome-->
         </button>
+        
+        <div class="search-wrap"> <!--This is a wrapper for styling purpose of the Search Bar-->
+            <!--fa-magnifying-glass is a Magnifying Glass Icon linked from Font Awesome-->
+            <i class="fa-solid fa-magnifying-glass"></i> <!--This creates a Magnifying Glass Icon which is just purely visual for now-->
+            <input type="text" placeholder="Search" aria-label="Search (visual only)">
+        </div>
+    </div>
