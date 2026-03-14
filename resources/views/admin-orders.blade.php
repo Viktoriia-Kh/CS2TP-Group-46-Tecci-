fb5582a5492bc5b7c114e2b1fbb941a4a7f74481
Ramsha Ahmed <rahmed0204@outlook.com>
2026-03-14 16:05:32 +0000

    Added Default HTML Code, And Links To CSS File, Fonts And Icons

---
+++ resources/views/admin-orders.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Tecci | Admin Dashboard</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- Shared Admin CSS -->
+  <link rel="stylesheet" href="TP2_Admin_Common.css">
+  <link rel="stylesheet" href="TP2_Admin_Orders.css">
+  <!--Google Font-->
+  <link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet" />
+  <!--Font Awesome for Icons-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+  
+</head>
