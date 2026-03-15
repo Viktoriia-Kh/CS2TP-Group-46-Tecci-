2b09fe213f122e2d9dd765c24cbebddbda5a29c9
Ramsha Ahmed <rahmed0204@outlook.com>
2026-03-15 01:17:59 +0000

    Added Default HTML Code, And Links The CSS File Fonts, And Icons

---
+++ resources/views/admin-common.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Tecci | Admin Dashboard</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- Shared Admin CSS -->
+  
+  <!--Google Font-->
+  <link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet" />
+  <!--Font Awesome for Icons-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+  
+</head>
+
+<body>
