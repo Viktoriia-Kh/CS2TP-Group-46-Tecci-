a280e0e014b497593155b2bdfc07c17392d2df07
Ramsha Ahmed <rahmed0204@outlook.com>
2026-03-17 01:25:13 +0000

    Added JS For The Rotating Banner Which Includes Creating A Function That Updates The Slider Number

--- public/home-page.js
+++ public/home-page.js
@@ -9,10 +9,19 @@ if (heroTrack && heroNextBtn) {
     let currentSlide = 0;
     const totalSlides = 3; /* This stores how many slides there are in total */
     let autoRotate; /* This declares a variable that will later store the interval timer ID */
-}
 
 /* This creates a function which performs the actual visual movement of the slider */
     const updateHeroSlide = () => {
         /* This moves the hero track horizontally using CSS transform */
         heroTrack.style.transform = `translateX(-${currentSlide * 100}%)`;
     };
+
+/* This creates a funtion which updates the slide number an then moves the banner */
+    const goToNextSlide = () => {
+        /* This increases the current slide by 1, if it reaches the end, it loops back to 0 */
+        currentSlide = (currentSlide + 1) % totalSlides;
+        /* This tells the browser to actually display the new slide */
+        updateHeroSlide();
+    };
+
+}
