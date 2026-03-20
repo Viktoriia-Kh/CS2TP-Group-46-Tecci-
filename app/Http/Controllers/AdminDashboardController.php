1470a878467275b70d93f03a203a55f440422fb4
Manrohit Bahia <msbahia11@outlook.com>
2026-03-20 15:08:21 +0000

    modified controller to show items that are out of stock and orders that need to be processed

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\OrderItem; 
+use App\Models\OrderItem;
 use Carbon\Carbon;
 
 class AdminDashboardController extends Controller
@@ -15,19 +15,27 @@ class AdminDashboardController extends Controller
         $revenueMonth = \App\Models\Order::whereMonth('created_at', \Carbon\Carbon::now()->month)
         ->whereYear('created_at', \Carbon\Carbon::now()->year)
         ->sum('total_price');
-        
+
         $refundRequests = OrderItem::whereNotNull('return_status')
             ->where('return_status', '!=', 'none')
             ->orderBy('updated_at', 'desc')
             ->take(5)
             ->get();
 
-        return view('admin-dashboard', compact('refundRequests', 'salesToday', 'revenueMonth'));
+        // orders that need to be processed
+        $pendingOrders = \App\Models\Order::where('status', 'Pending')->latest()->get();
+
+        // show items that are out of stock
+        $itemsOutOfStock = \App\Models\Inventory::where('quantity', '<=', 0)
+            ->with('product') // gets the product name from the database
+            ->get();
+
+        return view('admin-dashboard', compact('refundRequests', 'salesToday', 'revenueMonth', 'pendingOrders', 'itemsOutOfStock'));
     }
 
     public function approveReturn($id){
     $item = \App\Models\OrderItem::findOrFail($id);
-    
+
     $item->return_status = 'Approved';
     $item->save();
 
