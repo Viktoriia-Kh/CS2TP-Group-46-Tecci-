4b43dcf9d6c9efb7d2ad8eece7c4494e8c86e50c
ibrahim3103 <muhammadibrahim9087654@gmail.com>
2026-03-22 21:08:16 +0000

    fixed ui made the logo better and made it just look better

--- resources/views/admin/contacts.blade.php
+++ resources/views/admin/contacts.blade.php
@@ -6,7 +6,7 @@
   <title>Tecci | Contact Messages</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-  <link rel="stylesheet" href="{{ asset('admin-common-style.css') }}" />
+    <link rel="stylesheet" href="{{ asset('adminstyles.css') }}" />
   <link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
 </head>
