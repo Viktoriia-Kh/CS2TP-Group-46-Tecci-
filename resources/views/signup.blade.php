a03de9b92d0ef037d088438dfe96f36d1a035099
Ramsha Ahmed <rahmed0204@outlook.com>
2026-03-22 20:36:47 +0000

    Added The Links To The Dark Mode CSS File And The Dark Mode JavaScript File For The Signup Page

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -15,6 +15,7 @@
 
 <!-- Page specific styles SECOND -->
 <link rel="stylesheet" href="{{ asset('css/sign-up2style.css') }}">
+<link rel="stylesheet" href="Dark-Mode.css">
 
 <!-- Icons -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
@@ -178,6 +179,8 @@
     </div>
 </footer>
     @include('partials.chatbot')
-    <script src="{{ asset('chatbot.js') }}?v={{ time() }}"></script>
+    <script src="{{ asset('chatbot.js') }}?v={{ time() }}">
+            src="Dark-Mode-Theme.js"
+    </script>
 </body>
 </html>
